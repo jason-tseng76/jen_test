886ff3a58b53ef3b957a6ac42a42765017aaf380
Jason <jason.tseng76@gmail.com>
2016-05-26 17:13:46 +0800

    init

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3001;
+process.env.NODE_ENV = app.get('env');
+console.log('NODE_ENV='+process.env.NODE_ENV);
+
+var helmet = require('helmet');
+app.use(helmet());
+
+app.all('/', function(req, res) {
+	res.send('...');
+});
+app.all('/jason', function(req, res) {
+	console.log(req.path);
+	res.send('Jason index ...');
+});
+
+app.listen(port, function () {
+	console.log('Listening on port : ' + port);
+});
+
